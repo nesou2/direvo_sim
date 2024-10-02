35151722fe32002c378e7c6cf0a0b68febf5ac15
Jessica M James <119608532+jessmjames@users.noreply.github.com>
2024-10-02 10:21:26 +0100

    Add files via upload

--- strategy_sweep.py
+++ strategy_sweep.py
@@ -55,11 +55,13 @@ def directedEvolution(rng,
     return results
 
 thresholds, base_chances = base_chance_threshold_fixed_prop([0,0.19], 0.2, 5)
-splits = [20,10,4,2,1]
+splits = [100,50,5,2,1]
 muts = [0.01,0.1,0.5]
 pops = [5000, 1000, 100]
 Ns = [50,25,10]
-K_ratio = [0.5,0.25,0]
+K_ratio = [0.2, 0.1,0]
+
+reps = 1
 
 sweep_results = np.zeros((3,3,3,3,5,5))
 total_iterations = len(pops) * len(muts) * len(Ns) * len(np.array(K_ratio) * max(Ns)) * len(splits) * len(base_chances)
@@ -72,27 +74,35 @@ with tqdm.tqdm(total=total_iterations, desc="Overall Progress") as pbar:
                     for v, s in enumerate(splits):
                         for vi, (bc, th) in enumerate(zip(base_chances, thresholds)):
 
-                            split_results = []
-                            for split in range(s):
-                                params = {'threshold': th, 'base_chance' : bc}
-                                rng = jr.PRNGKey(42)
-                                run = directedEvolution(rng, 
-                                                        N = N, 
-                                                        K=K, 
-                                                        selection_strategy=slct.base_chance_threshold_select, 
-                                                        selection_params = params, 
-                                                        popsize=int(p/s), 
-                                                        mut_chance=m, 
-                                                        num_steps=100, 
-                                                        num_reps=1, 
-                                                        average=True)
-                            
-                                split_results.append(run['fitness'].max(axis=2).mean(axis=0)[-1])
+                            repeat_results = []
+                            rng_key = jr.PRNGKey(42)
+                            rep_rngs = jr.split(rng_key, reps)
 
-                            sweep_results[i,ii,iii,iv,v,vi]
+                            for rng in rep_rngs:
+                                split_rngs = jr.split(rng, s)
+                                split_results = []
+                                for split in range(s):
+                                    params = {'threshold': th, 'base_chance' : bc}
+                                    run = directedEvolution(split_rngs[s], 
+                                                            N = N, 
+                                                            K=K, 
+                                                            selection_strategy=slct.base_chance_threshold_select, 
+                                                            selection_params = params, 
+                                                            popsize=int(p/s), 
+                                                            mut_chance=m/N, 
+                                                            num_steps=100, 
+                                                            num_reps=40, 
+                                                            average=True)
+                                
+                                    #split_results.append(run['fitness'].max(axis=2).mean(axis=0)[-1])
+                                    split_results.append(run['fitness'][:,:,-1].max(axis=1).mean())
+
+                                repeat_results.append(np.array(split_results).max())
+
+                            sweep_results[i,ii,iii,iv,v,vi] = np.mean(np.array(repeat_results))
 
                             # Update progress bar
                             pbar.update(1)
 
-with open('strategy_sweep.pkl', 'wb') as f:
+with open('strategy_sweep4_fixedrngs.pkl', 'wb') as f:
     pickle.dump(sweep_results, f)
